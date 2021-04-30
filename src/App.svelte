7381db0528617ac4e96997526b704d6516b5a2f7
andynet62 <info@sartoriandrea.com>
2021-04-30 21:44:40 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+
+<script>
+        let count=0
+</script>
+
+<button on:click={()=>count++}>
+        Increment
+</button>
+<button on:click={()=>count--}>
+        Decrement
+</button>
+<button on:click={()=>count=0}>
+        Reset
+</button>
+
+<h2>
+        {count}
+</h2>
